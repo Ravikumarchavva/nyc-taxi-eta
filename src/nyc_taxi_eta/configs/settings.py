3fa565e17f786e9ed0bc1240d85c660cf5bbc497
Ravikumarchavva <ravikumarchavva@outlook.com>
2025-11-30 14:10:13 +0530

    sennt data to unity cataog 1 pass

--- src/nyc_taxi_eta/configs/settings.py
+++ src/nyc_taxi_eta/configs/settings.py
@@ -1,20 +1,18 @@
 from pathlib import Path
-
+from dotenv import load_dotenv
+import os
+load_dotenv()
 # Define the directories
 ROOT_DIR = Path(__file__).resolve().parent.parent.parent.parent
 DATA_DIR = ROOT_DIR / "data"
 LANDING_DIR = DATA_DIR / "landing"
 YELLOW_TAXI_DIR = LANDING_DIR / "trips_data" / "yellow_taxi"
 
-# # # Helper function to get sorted .parquet files
-# def get_parquet_files(dir_path):
-#     return sorted([os.path.join(dir_path, file) for file in os.listdir(dir_path) if file.endswith('.parquet')])
+WAREHOUSE_DIR = DATA_DIR / "metastore"
+BRONZE_DIR = WAREHOUSE_DIR / "bronze"
+SILVER_DIR = WAREHOUSE_DIR / "silver"
+GOLD_DIR = WAREHOUSE_DIR / "gold"
+ML_DIR = WAREHOUSE_DIR / "ml"
 
-# # Get the lists of .parquet files
-# RAW_FILE_LIST = get_parquet_files(RAW_DATA_DIR)
-# PROCESSED_FILE_LIST = get_parquet_files(PROCESSED_DATA_DIR)
-
-
-# if __name__ == '__main__':
-#     print(RAW_FILE_LIST)
-#     print(PROCESSED_FILE_LIST)
+DATABASE_USER=os.getenv("DATABASE_USER")
+DATABASE_PASSWORD=os.getenv("DATABASE_PASSWORD")
